d36511c7f0546e02fca8a128040a3c855184e3b4
uraboer <493970240@qq.com>
2018-03-02 14:35:20 +0800

    20180302

--- book_hive_20180228.R
+++ book_hive_20180228.R
@@ -1,1 +1,43 @@
 #Chapter 1
+
+#Chapter 2
+
+#Chapter 3 Data Types and File Formats
+
+#Chaoter 4 HiveQL:Data Definition
+
+#Hive offers no support for row-level inserts,updates,and deletes.
+#Hive doesn't support transactions.
+#Hive adds extensions to provide better performance in the context of Hadoop
+#and to integrate with custom extensions and even external programs.
+
+
+#>describe database XXX;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
